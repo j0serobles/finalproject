419432e55f566bfba44c92f916e2c85b56b33656
jrrobles <jrrobles@JRROBLES-US.us.oracle.com>
2019-11-05 00:23:26 -0500

    Configured for heroku deployment

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,8 @@
+//set the mongoURI to : 
+//mongoURI: "mongodb://localhost:27017/mern-passport",
+// to use local database
+
 module.exports = {
-    mongoURI: "mongodb://localhost:27017/mern-passport",
+    mongoURI: "mongodb://engjoserobles:Culipande0@ds141228.mlab.com:41228/heroku_sw64z731",
     cypher: "blahblah"
 };
--- server.js
+++ server.js
@@ -8,10 +8,14 @@ const users = require('./routes/api/users');
 const app = express();
 const port = process.env.PORT || 5000;
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build")); 
+}
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-const db = require("./config/keys").mongoURI;
+const db = process.env.MONGODB_URI || require("./config/keys").mongoURI;
 mongoose.connect(db, { useNewUrlParser: true }).then(() => console.log("MongoDB connected")).catch((err) => console.log(err));
 
 app.use(passport.initialize());
