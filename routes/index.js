006e8e8b4301a1fdb048abb916a1743154a05905
joraro <engjoserobles@gmail.com>
2019-11-07 15:23:01 -0500

    Default non found express routes to react router.

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const apiRoutes      = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
-// If no API or users routes are hit, send the React app
+// If no API routes are hit, send to the React router
 router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.redirect("/");
 });
 
 module.exports = router;
